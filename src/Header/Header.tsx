f6907ea4dc6c014c22eaa87e267aabfeb5280fb1
Daniele Marchetta <danymar20.9@gmail.com>
2023-04-16 19:47:35 +0200

    wip

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Center, Image, useColorMode } from "@chakra-ui/react"
 const Header = () => {
     const { colorMode } = useColorMode()
     return (<Center >
-        <Image src={colorMode==='light'?'/LogoHeader.png':'LogoHeaderWhite.png'} w={{ base: "125px", md: "269px" }} h={{ base: "35px", md: "76px" }} my={{ base: "2rem", md: 12 }}></Image>
+        <Image src={colorMode==='light'?'/logoHeader.png':'LogoHeaderWhite.png'} w={{ base: "125px", md: "269px" }} h={{ base: "35px", md: "76px" }} my={{ base: "2rem", md: 12 }}></Image>
     </Center>
     )
 }

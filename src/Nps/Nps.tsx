cba71bb8087a0a62f703bd1ca90b0540aa71d53f
Daniele Marchetta <danymar20.9@gmail.com>
2023-04-16 11:53:38 +0200

    wip

--- src/Nps/Nps.tsx
+++ src/Nps/Nps.tsx
@@ -2,10 +2,13 @@ import {
   Box,
   Button,
   Center,
+  Container,
+  Divider,
   Flex,
   FormControl,
   Radio,
   RadioGroup,
+  Stack,
   Text,
   useBoolean,
 } from "@chakra-ui/react";
@@ -28,17 +31,19 @@ const Nps = ({ questions, answers, actual ,increment }: { questions: string[], a
     {questions.map((q, index) => {
         return (
           <Box key={index} mx={10} display={questions[actual]===q?"-moz-initial":"none"}>
-            <Flex justifyContent={["flex-start", "flex-start", "center"]} mt={{base:"1rem",md:"2rem",lg:"3rem"}} mb={{base:"1.5rem",md:"3rem",lg:"4.2rem"}}>
-        <Text  textAlign={"center"} as={"h1"} fontWeight={"bold"} fontSize={{ base: "20", md: "40px" }}>{q}</Text>
+            <Flex justifyContent={["flex-start", "flex-start", "center"]} mt={{base:"1rem",md:"2rem",lg:"3rem"}} >
+        <Text  textAlign={"left"} as={"h1"} fontWeight={"bold"} fontSize={{ base: "20", md: "40px" }}>{q}</Text>
       </Flex>
-      <FormControl  px={{ md: 20 }}>
+      <Box height={"1px"} my={{base:"1.5rem",md:"3rem",lg:"4.2rem"}}bgColor={"black"} visibility={{base:"visible",md:"hidden"}}  w={"20%"}>
+      </Box>
+            <FormControl  px={{ md: 20 }}>
         <RadioGroup onChange={setFlag.off} variant={"none"}  name={q} id={q} w={"full"} >
           <Flex direction={["column", "column", "row"]} alignItems={["flex-start", "flex-start", "center"]}  >
             {answers[index].map((answer, index) => {
               return (
                 <Flex key={index} flexGrow={3}  flexDir={["row", "row", "column", "column", "column", "column"]} justifyContent={"center"} alignItems={"center"} gap={[2, 2, 5]}>
                   <Radio borderColor={"black"}  value={answer} _first={LabelStyles} _checked={{ "borderColor": "black", "borderWidth": "5px" }}  />
-                  <Text fontSize={["18px", "18x", "18px","25px"]} maxW={{ md: "150px" }} wordBreak={"break-word"} alignItems={"center"} height={{ md: "48px" }} textAlign={"center"} >
+                  <Text fontSize={["18px", "18x", "18px","25px"]} maxW={{ md: "150px" }} wordBreak={"break-word"} alignItems={"center"} height={{ md: "48px" }} textAlign={{base:"left",md:"center"}} >
                     {answer}
                   </Text>
                 </Flex>
